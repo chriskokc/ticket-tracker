21db9a64662a7332cb55bcaa8985233a043c30f6
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-19 14:24:26 +0100

    added Card component and some more styles to TicketTracker.

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.scss";
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h3 className="card__content">Name</h3>
+            <h3 className="card__content">Role</h3>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/TicketTracker/TicketTracker.jsx
+++ src/components/TicketTracker/TicketTracker.jsx
@@ -1,8 +1,25 @@
+import Card from "../Card/Card";
 import "./TicketTracker.scss";
 
 const TickerTracker = () => {
     return (
-        <h1 className="big-title">Ticket Tracker</h1>
+        <div className="ticker-tracker">
+            <h1 className="big-title">Ticket Tracker</h1>
+            <div className="ticker-tracker__container">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            
+        </div>
+        
     );
 };
 
