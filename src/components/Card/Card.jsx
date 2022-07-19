4662ddfdcfe6661aede6d716a9fb127a19fd88b5
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-19 15:07:29 +0100

    added Counter component and some styles.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import "./Card.scss";
+import Counter from "../Counter/Counter.jsx";
 
 const Card = () => {
     return (
         <div className="card">
             <h3 className="card__content">Name</h3>
             <h3 className="card__content">Role</h3>
+            <Counter />
         </div>
     );
 };
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,12 @@
+import "./Counter.scss";
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <h3 className="counter__title">Counter</h3>
+            <h3 className="counter__value">0</h3>
+        </div>
+    );
+};
+
+export default Counter;
